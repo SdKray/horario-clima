27e10204a0ee435871c54fa2aa156262abc6664b
Luis Alberto Baca Cortes <15200558@gmail.com>
2020-03-06 15:00:36 -0600

    Se agrego el consumo de la api del clima

--- src/components/Hora.js
+++ src/components/Hora.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default class Hora extends Component {
-	render() {
-		let d = new Date();
-		return (
-			<div className='rectangle'>
-				<div className='container'>
-					<span className='hora'>
-						{d.getHours()}:{d.getMinutes()}
+export default function Hora() {
+	let [clima, obtnerClima] = useState([]);
+	let d = new Date();
+	let key = '7244ae991b10464396e1125c249da989';
+	let consultarApi = async () => {
+		let url = `https://api.weatherbit.io/v2.0/current?city=Pachuca&key=${key}`;
+		let res = await fetch(url);
+		let clima = await res.json();
+		obtnerClima(clima.data);
+	};
+	useEffect(() => {
+		consultarApi();
+	}, []);
+	console.log(clima);
+	return (
+		<div className='rectangle'>
+			<div className='container'>
+				<span className='hora'>
+					{d.getHours()}:{d.getMinutes()}
+				</span>
+				<div>
+					<span className='fecha'>
+						Miércoles {d.getDate()} de {d.getMonth() + 1} del {d.getFullYear()}
 					</span>
-					<div>
-						<span className='fecha'>
-							Miércoles {d.getDate()} de {d.getMonth() + 1} del {d.getFullYear()}
-						</span>
-					</div>
-				</div>
-				<div className='container'>
-					<span className='grados'>31º</span>
-					<span className='cuidad'>Pachuca</span>
 				</div>
 			</div>
-		);
-	}
+			<div className='container'>
+				{clima.length > 0 ? <span className='grados'>{clima[0].temp}º</span> : null}
+				<span className='cuidad'>Pachuca</span>
+			</div>
+		</div>
+	);
 }
